adc9e134e780c3b6d73314a3ba617cd8ace68602
Camdyn <camdyn3@protonmail.com>
2022-02-10 15:20:02 -0800

    add express listen

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ const express = require("express");
 const app = express();
 
 const queryFilter = require("./queryFilter.js");
+
+
+app.listen(3000, () => {
+    console.log("listening on p 3000")
+})
