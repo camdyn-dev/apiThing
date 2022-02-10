55e53fea94e63242b715e78a7e512d2e4f49eb12
Camdyn <camdyn3@protonmail.com>
2022-02-09 19:23:40 -0800

    fix variable names

--- queryFilter.js
+++ queryFilter.js
@@ -1,4 +1,4 @@
-let string = "?type=fart&poop=shit&cum=fart"
+let string = "?type=wappa&language=yiddish&pee=pee"
 let total = [0]
 for(let i = 0; i < string.length; i++){
     if(string[i] === "&" || string[i] === "="){
@@ -12,14 +12,15 @@ for(let i = 0; i < string.length; i++){
 let v = 0
 let i = 1
 for(i; i < total.length; i += 2){
-    const num1 = (total[v]) + 1
-    const num2 = (total[i])
-    console.log(num1, num2)
-    const uno = string.slice(num1, num2)
-    const num3 = num2 + 1
-    const num4 = (total[i + 1])
-    const dos = string.slice(num3, num4)
-    console.log(uno)
-    console.log(dos)
+    const index1 = (total[v]) + 1
+    const index2 = (total[i])
+    console.log(index1, index2)
+    const key = string.slice(index1, index2)
+    const index3 = index2 + 1
+    const index4 = (total[i + 1])
+    const value = string.slice(index3, index4)
+    console.log(key, value)
+
     v += 2
 }
+//after careful consideration, it seems this does not workie
