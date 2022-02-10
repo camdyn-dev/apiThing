a9eae1cf8d6794a26968b6a7e56749fd6608e11c
Camdyn <camdyn3@protonmail.com>
2022-02-09 21:32:04 -0800

    put queryFilter into function, document it and add to app.js

--- queryFilter.js
+++ queryFilter.js
@@ -1,30 +1,38 @@
-let string = "?type=wappa&language=yiddish&pee=pee"
-let total = [0]
-for(let i = 0; i < string.length; i++){
-    if(string[i] === "&" || string[i] === "="){
-        total.push(i)
+let params = "?type=wappa&language=yiddish&pee=pee"
+
+const queryFilter = (params) => {
+
+let indexes = [0]
+for(let i = 0; i < params.length; i++){
+    if(params[i] === "&" || params[i] === "="){
+        indexes.push(i) //push the location of the identifier into the index list
     }
 }
 
-const params = {}
-//holy fuck this shit workie
-//so, gonna use this to filter a query string
+const returnValue = {}
+
+//wish I could do this another way, but integer and value it is
 let v = 0
 let i = 1
-for(i; i < total.length; i += 2){
-    const index1 = (total[v]) + 1
-    const index2 = (total[i])
-    const key = string.slice(index1, index2)
 
-    const index3 = index2 + 1
-    const index4 = (total[i + 1])
-    const value = string.slice(index3, index4)
+for(i; i < indexes.length; i += 2){
+    const index1 = (indexes[v]) + 1 //grab first letter of param
+    const index2 = (indexes[i]) //grab last letter of param
+    const key = params.slice(index1, index2) //slice the param out
 
-    params[key] = value
+    const index3 = index2 + 1 //grab first letter of argument
+    const index4 = (indexes[i + 1]) //grab second letter of argument
+    const value = params.slice(index3, index4) //slice the argument out
+
+    returnValue[key] = value //add it to the returnValue object
 
     v += 2
+    //i and v are incremented by two because there are half the amount of outputs as there are inputs
 }
-//after careful consideration, it seems this does not workie
+return returnValue
+}
 
-console.log(params)
-//after some more careful consideration and a good amount of googling, i think it workie
+const newParams = queryFilter(params)
+console.log(newParams)
+
+module.exports = queryFilter()
