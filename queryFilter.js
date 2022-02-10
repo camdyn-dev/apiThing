65a6ff4aa7858fcdf70309ef3f537f7c9fe859d0
Camdyn <camdyn3@protonmail.com>
2022-02-09 20:12:55 -0800

    fix(?) filter logic

--- queryFilter.js
+++ queryFilter.js
@@ -6,7 +6,7 @@ for(let i = 0; i < string.length; i++){
     }
 }
 
-
+const params = {}
 //holy fuck this shit workie
 //so, gonna use this to filter a query string
 let v = 0
@@ -14,13 +14,17 @@ let i = 1
 for(i; i < total.length; i += 2){
     const index1 = (total[v]) + 1
     const index2 = (total[i])
-    console.log(index1, index2)
     const key = string.slice(index1, index2)
+
     const index3 = index2 + 1
     const index4 = (total[i + 1])
     const value = string.slice(index3, index4)
-    console.log(key, value)
+
+    params[key] = value
 
     v += 2
 }
 //after careful consideration, it seems this does not workie
+
+console.log(params)
+//after some more careful consideration and a good amount of googling, i think it workie
