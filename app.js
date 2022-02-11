f6f401ca53e95109ccb0da7eca2f6621f779c000
Camdyn <camdyn3@protonmail.com>
2022-02-10 19:05:35 -0800

    add mongoose and start seeds

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
+//express stuff
 const express = require("express");
 const app = express();
 
+//mongoose stuff
+const mongoose = require('mongoose');
+main().catch(err => console.log(err));
+async function main() {
+    await mongoose.connect('mongodb://localhost:27017/projectAPI');
+}
+
+
 //I am so upset that there's a built in query parser that I did not find before I wrote mine M A D G E
-const queryFilter = require("./queryFilter.js");
+// const queryFilter = require("./queryFilter.js");
 
 app.get("/project", (req, res) => {
     const options = req.query
