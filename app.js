47cd1cf28e060ae5daf5bc78adebbcfa758d06b2
Camdyn <camdyn3@protonmail.com>
2022-02-10 19:02:11 -0800

    implement req.query instead of filter MADGE

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
 
+//I am so upset that there's a built in query parser that I did not find before I wrote mine M A D G E
 const queryFilter = require("./queryFilter.js");
 
+app.get("/project", (req, res) => {
+    const options = req.query
+    console.log(options)
+    res.send(options)
+})
 
 app.listen(3000, () => {
     console.log("listening on p 3000")
 })
+
