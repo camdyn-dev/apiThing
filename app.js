2234df91ecb3fcc2b979dcf127b012c98b16837f
Camdyn <camdyn3@protonmail.com>
2022-02-11 17:48:22 -0800

    add rate limiter

--- app.js
+++ app.js
@@ -1,28 +1,39 @@
-//express stuff
+//packages
 const express = require("express");
 const app = express();
+const rateLimit = require("express-rate-limit")
+const mongoose = require('mongoose');
+const mongoSanitize = require("express-mongo-sanitize")
 
+//models
+const Project = require("./models/project.js")
+const sample = require("./utilities/sample.js")
 
 //mongoose stuff
-const mongoose = require('mongoose');
 main().catch(err => console.log(err));
 async function main() {
     await mongoose.connect('mongodb://localhost:27017/projectAPI');
 }
 
 
-const Project = require("./models/project.js")
-const sample = require("./utilities/sample.js")
+const apiLimit = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 25,
+    message: "You've sent too many requests! Please wait about 15 minutes before trying again.",
+    standardHeaders: true,
+    legacyHeaders: false,
+})
 
 
-const mongoSanitize = require("express-mongo-sanitize")
+
+
 app.use(mongoSanitize())
 
 
-app.get("/project", async (req, res) => {
+app.get("/project", apiLimit, async (req, res) => {
     const options = req.query
 
-    if ("type" in options || "level" in options || "lang" in options) { //this will be a bitch to expand; makes sure at least one param is in there
+    if ("type" in options || "level" in options || "lang" in options || "any" in options) { //this will be a bitch to expand; makes sure at least one param is in there
         console.log(options)
 
         const project = await Project.find(options)
